da14562a33f6d81ba87e7a283a4f74271c538a05
bvujovic <bv.net@outlook.com>
2016-03-12 00:25:09 +0100

    commit 01

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Met Hotels - Bojan Vujović 1859</title>
+        <style>
+            body { font-family: 'Segoe UI', 'Arial', sans-serif; }
+        </style>
+    </head>
+    <body>
+        <h1>Met Hotels</h1>
+
+        <?php
+        echo 'Sajt u izradi...';
+        ?>
+    </body>
+</html>
